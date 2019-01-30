8f6914e122f03e8403b282e88efa502708750090
Frank Martinez <frankie.martinez@gmail.com>
2019-01-30 11:19:40 -0800

    Removed the 'CSS' folder (leaving only 'css') to reduce confusion for anyone who would work on this project moving forward. They seemed to be identical.

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,10 +1,37 @@
+// // How this should go in my mind.
+// class Menu {
+//   constructor(element) {
+//     this.menu = element.querySelector('.menu');
+//     console.log(menu);
+//     this.menu.addEventListener('click', () => this.toggleMenu());
+//   }
+//   toggleMenu() {
+//     this.menu.classList.toggle('.menu--open');
+//   }
+// }
+
+// const menu = document.querySelector('.menu');
+// const menuButton = document.querySelector('.menu-button');
+// const menuToggle = new Menu(menu);
+
+// console.log(menuToggle)
+
+
+
 
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence. 
+  // Toggle the "menu--open" class on your menu refence.
+  this.classList.toggle('menu--open');
+  console.log('hello')
 }
 
 // Start Here: Create a reference to the ".menu" class
-const menu;
+const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
-const menuButton;
+const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
+menuButton.addEventListener('click', () => {
+  console.log('I\'m trying to Work');
+  menuButton.toggleMenu();
+})
+
