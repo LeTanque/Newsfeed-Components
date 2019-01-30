f10767b12063471b75ec87dcd6297b9c6eb5431d
Frank Martinez <frankie.martinez@gmail.com>
2019-01-30 15:48:42 -0800

    MVP with some stretch completed

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,28 +1,6 @@
-// // How this should go in my mind.
-// class Menu {
-//   constructor(element) {
-//     this.menu = element.querySelector('.menu');
-//     console.log(menu);
-//     this.menu.addEventListener('click', () => this.toggleMenu());
-//   }
-//   toggleMenu() {
-//     this.menu.classList.toggle('.menu--open');
-//   }
-// }
-
-// const menu = document.querySelector('.menu');
-// const menuButton = document.querySelector('.menu-button');
-// const menuToggle = new Menu(menu);
-
-// console.log(menuToggle)
-
-
-
-
-const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-  this.classList.toggle('menu--open');
-  console.log('hello')
+const toggleMenu = (element) => {
+  // // Toggle the "menu--open" class on your menu refence.
+  element.classList.toggle('menu--open');
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -30,8 +8,5 @@ const menu = document.querySelector('.menu');
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu
-menuButton.addEventListener('click', () => {
-  console.log('I\'m trying to Work');
-  menuButton.toggleMenu();
-})
+menuButton.addEventListener('click', () => { toggleMenu(menu); })
 
